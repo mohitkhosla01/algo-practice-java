79878ff17e6ae06aee23f071521c4adf019b07fa
mohitkhosla01 <mohit.khosla.01@gmail.com>
2019-09-03 09:21:12 +0530

    Added 'ReverseString' to Leetcode

--- leetcode/easy/ReverseString.java
+++ leetcode/easy/ReverseString.java
@@ -4,21 +4,27 @@ public class ReverseString {
 
 	public static void main(String[] args) {
 		
-		char[] s = {'h','e','l','l','o'};
-		// char[] s = {"H","a","n","n","a","h"};
+		// char[] s = {'h','e','l','l','o'};
+		// char[] s = {'H','a','n','n','a','h'};
+		// char[] s = {'H'};
+		// char[] s = null;
+		char[] s = {'A','B'};
 		
 		new ReverseString().reverseString(s);
+		
+    	for(int i=0; i<s.length; ++i) {
+    		System.out.print(s[i] + " ");
+    	}
 	}
 	
     public void reverseString(char[] s) {
     	
-    	for(int i=0; i<s.length/2; ++i) {
-    		s[i] = s[s.length - i - 1];
-    		s[s.length - i - 1] = s[i];
-    	}
-    	
-    	for(int i=0; i<s.length; ++i) {
-    		System.out.print(s[i] + " ");
+    	if(s != null) {
+        	for(int i=0; i<s.length/2; ++i) {
+        		char ch = s[s.length - i - 1];
+        		s[s.length - i - 1] = s[i];
+        		s[i] = ch;
+        	}
     	}
     }
 }
