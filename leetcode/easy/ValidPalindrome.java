f7f02d31a30eb375fee62688fc6e45a0fb15bc63
mohitkhosla01 <mohit.khosla.01@gmail.com>
2019-09-04 08:38:43 +0530

    Improved 'ValidPalindrome' in Leetcode

--- leetcode/easy/ValidPalindrome.java
+++ leetcode/easy/ValidPalindrome.java
@@ -1,21 +1,21 @@
 package easy;
 
-import java.util.ArrayList;
-
 public class ValidPalindrome {
 
 	public static void main(String[] args) {
 		
-		// String s = "A man, a plan, a canal: Panama";
-		// String s = "race a car";
-		// String s = "mohit";
-		// String s = "tomamot";
-		// String s = "to Mamo,t";
-		// String s = "t3o Mamo,2t";
-		// String s = "t2o Mamo,2t";
-		// String s = "";
-		String s = null;
-
+		// String s = "A man, a plan, a canal: Panama"; // true
+		// String s = "race a car"; // false
+		// String s = "mohit"; // false
+		// String s = "tomamot"; // true
+		// String s = "to Mamo,t"; // true
+		// String s = "t3o Mamo,2t"; // false
+		// String s = "t2o Mamo,2t"; // true
+		// String s = ""; // true
+		// String s = null; // true
+		// String s = "a."; // true
+		String s = "0P"; // false
+		
 		System.out.println(new ValidPalindrome().isPalindrome(s));
 	}
 	
@@ -24,24 +24,59 @@ public class ValidPalindrome {
     	if(s == null) {
     		return true;
     	}
-        
-    	ArrayList<Character> arr = new ArrayList<Character>();
     	
-    	for(int i=0; i<s.length(); ++i) {
-    		char ch = s.charAt(i);
-    		if((ch >= 48 && ch <= 57) || (ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) {
-    			if(ch >= 65 && ch <= 90) {
-    				arr.add((char)((int)ch + 32));
-    			}
-    			else {
-    				arr.add(ch);
-    			}
+    	int i=0;
+    	int j=s.length()-1;
+    	
+    	while(i<s.length() && j>=0) {
+    		char chi = s.charAt(i);
+    		char chj = s.charAt(j);
+    		
+    		boolean flag = true;
+    		
+    		if(!((chi >= 48 && chi <= 57) || (chi >= 65 && chi <= 90) || (chi >= 97 && chi <= 122))) {
+    			++i;
+    			flag = false;
     		}
-    	}
-    	
-    	for(int i=0; i<arr.size()/2; ++i) {
-    		if(arr.get(i) != arr.get(arr.size()-i-1)) {
-    			return false;
+    		if(!((chj >= 48 && chj <= 57) || (chj >= 65 && chj <= 90) || (chj >= 97 && chj <= 122))) {
+    			--j;
+    			flag = false;
+    		}
+    		
+    		if(flag == true) {
+    			if((chi >= 48 && chi <= 57) && (chj >= 48 && chj <= 57)) {
+    				if(chi != chj) {
+    					return false;
+    				}
+    			}
+    			if((chi >= 48 && chi <= 57) && !(chj >= 48 && chj <= 57)) {
+    				return false;
+    			}
+    			if(!(chi >= 48 && chi <= 57) && (chj >= 48 && chj <= 57)) {
+    				return false;
+    			}
+    			else if((chi >= 65 && chi <= 90) && (chj >= 65 && chj <= 90)) {
+    				if(chi != chj) {
+    					return false;
+    				}
+    			}
+    			else if((chi >= 97 && chi <= 122) && (chj >= 97 && chj <= 122)) {
+    				if(chi != chj) {
+    					return false;
+    				}
+    			}
+    			else if((chi >= 65 && chi <= 90) && (chj >= 97 && chj <= 122)) {
+    				if((char)((int)chi + 32) != chj) {
+    					return false;
+    				}
+    			}
+    			else if((chi >= 97 && chi <= 122) && (chj >= 65 && chj <= 90)) {
+    				if(chi != (char)((int)chj + 32)) {
+    					return false;
+    				}
+    			}
+    			++i;
+    			--j;
     		}
     	}
     	
