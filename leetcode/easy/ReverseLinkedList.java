35f8992d5445cfea957b3005e16e91581700470f
mohitkhosla01 <mohit.khosla.01@gmail.com>
2019-09-05 09:10:22 +0530

    Made minor modifications to 'ReverseLinkedList' in Leetcode

--- leetcode/easy/ReverseLinkedList.java
+++ leetcode/easy/ReverseLinkedList.java
@@ -6,20 +6,20 @@ public class ReverseLinkedList {
 
 	public static void main(String[] args) {
 
-		//	ListNode head = new ListNode(1);
-		//	head.next = new ListNode(2);
-		//	head.next.next = new ListNode(3);
-		//	head.next.next.next = new ListNode(4);
-		//	head.next.next.next.next = new ListNode(5);
+			ListNode head = new ListNode(1);
+			head.next = new ListNode(2);
+			head.next.next = new ListNode(3);
+			head.next.next.next = new ListNode(4);
+			head.next.next.next.next = new ListNode(5);
 
-		// 	ListNode head = null;
+		// ListNode head = null;
 		
-		//  ListNode head = new ListNode(8);
-		//  head.next = new ListNode(9);
+		// ListNode head = new ListNode(8);
+		// head.next = new ListNode(9);
 		
-		ListNode head = new ListNode(20);
+		// ListNode head = new ListNode(20);
 
-		ListNode node = new ReverseLinkedList().reverseList(head);
+		ListNode node = new ReverseLinkedList().reverseList(head.next.next);
 
 		while(node != null) {
 			System.out.print(node.val + " ");
