fdf3b1b27362fa6c5baafeab55c7b2fe9bf0fab6
mohitkhosla01 <mohit.khosla.01@gmail.com>
2019-09-21 11:42:09 +0530

    Added LargestNumber to Leetcode

--- interviewbit/arrays/LargestNumber.java
+++ interviewbit/arrays/LargestNumber.java
@@ -1,75 +1,81 @@
 package arrays;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class LargestNumber {
 
 	public static void main(String[] args) {
+
+		List<Integer> A = new ArrayList<Integer>();
+//		A.add(3);
+//		A.add(30);
+//		A.add(34);
+//		A.add(5);
+//		A.add(9);
+//		A.add(81);
 		
-		List<Integer> arr = new ArrayList<Integer>();
-		arr.add(3);
-		arr.add(30);
-		arr.add(34);
-		arr.add(5);
-		arr.add(9);
-		arr.add(81);
+//		A.add(8);
+//		A.add(89);
 		
-		new LargestNumber().largestNumber(arr);
-		System.out.println("AAA");
+//		A.add(0);
+//		A.add(0);
+//		A.add(0);
+//		A.add(0);
+//		A.add(0);
+		
+//		A.add(1);
+//		A.add(0);
+//		A.add(2);
+//		A.add(3);
+//		A.add(4);
+		
+		A.add(8);
+		A.add(0);
+		A.add(0);
+		A.add(89);
+
+		System.out.println(new LargestNumber().largestNumber(A));
 	}
-	
+
     public String largestNumber(final List<Integer> A) {
-    	
-    	LinkedList<String> li = new LinkedList<String>();
-    	li.add(""+A.get(0));
-    	
-    	for(int i=1; i<A.size(); ++i) {
-    		String t = ""+A.get(i);
-    		
-    		for(int j=i-1; j>=0; ++j) {
-    			if(li.get(j).charAt(0) > t.charAt(0)) {
-    				li.add(j+1, t);
-    			}
-    			else if(li.get(j).charAt(0) < t.charAt(0)) {
-    				li.add(j, t);
-    			}
-    			else if(li.get(j).charAt(0) == t.charAt(0)) {
-        			boolean breakFlag = false;
-        			for(int k=1; k<Math.max(li.get(j).length(), t.length()); ++k) {
-        				if(li.get(j).length() <= (k+1)) {
-        					li.add(j+1, t);
-        					breakFlag = true;
-        					break;
-        				}
-        				else if(t.length() <= (k+1)) {
-        					li.add(j, t);
-        					breakFlag = true;
-        					break;
-        				}
-        				else if(li.get(j).charAt(j) > t.charAt(j)) {
-        					li.add(j+1, t);
-        					breakFlag = true;
-        					break;
-        				}
-        				else if(t.charAt(j) > li.get(j).charAt(j)) {
-        					li.add(j, t);
-        					breakFlag = true;
-        					break;
-        				}
-        			} 
-        			if(!breakFlag) {
-        				li.add(j+1, t);
-        			}
-        		}
-    		}
-    	}
-		
-		for(int i=0; i<li.size(); ++i) {
-			System.out.println(li.get(i));
-		}
-    	
-    	return null;
+        
+    	boolean nonZero = false;
+        List<Integer> B = new ArrayList<Integer>();
+        for(int a : A) {
+        	if(a != 0) {
+        		nonZero = true;
+        	}
+        	B.add(a);
+        }
+        if(!nonZero) {
+        	B.clear();
+        	B.add(0);
+        }
+        
+        Collections.sort(B, new Comparator<Integer>(){
+            
+            @Override
+            public int compare(Integer x, Integer y) {
+                
+                String s1 = ""+x+y;
+                String s2 = ""+y+x;
+                if(s1.compareTo(s2) > 0) {
+                    return -1;
+                }
+                else {
+                    return 1;
+                }
+            }
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        for(int b : B) {
+            sb.append(b);
+        }
+        
+        return sb.toString();
     }
 }
