0d624d149bdd3a8ff26d947dae2d8a65d11e21a5
mohitkhosla01 <mohit.khosla.01@gmail.com>
2019-09-27 08:16:33 +0530

    Committing BinaryTreeZigZagLevelOrderTraversal - 2nd technique in Leetcode

--- leetcode/medium/BinaryTreeZigzagLevelOrderTraversal.java
+++ leetcode/medium/BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,39 +1,53 @@
 package medium;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Stack;
 
 import definition.TreeNode;
 
 public class BinaryTreeZigzagLevelOrderTraversal {
 
 	public static void main(String[] args) {
-		
-//		TreeNode root = new TreeNode(3);
-//		root.left = new TreeNode(9);
-//		root.right = new TreeNode(20);
-//		root.right.left = new TreeNode(15);
-//		root.right.right = new TreeNode(7);
-		
-//		TreeNode root = new TreeNode(2);
-//		root.left = new TreeNode(5);
-//		root.left.left = new TreeNode(4);
-//		root.left.left.left = new TreeNode(1);
-//		root.left.left.right = new TreeNode(3);
-//		root.left.right = new TreeNode(8);
-//		root.left.right.left = new TreeNode(9);
-//		root.left.right.right = new TreeNode(7);
-//		root.right = new TreeNode(20);
-//		root.right.left = new TreeNode(15);
-//		root.right.right = new TreeNode(12);
-		
-//		TreeNode root = new TreeNode(2);
-		
-		TreeNode root = null;
-		
-		
+
+		TreeNode root = new TreeNode(3);
+		root.left = new TreeNode(9);
+		root.right = new TreeNode(20);
+		root.right.left = new TreeNode(15);
+		root.right.right = new TreeNode(7);
+
+		//		TreeNode root = new TreeNode(2);
+		//		root.left = new TreeNode(5);
+		//		root.right = new TreeNode(20);
+		//		root.left.left = new TreeNode(4);
+		//		root.left.right = new TreeNode(8);
+		//		root.right.left = new TreeNode(15);
+		//		root.right.right = new TreeNode(12);
+		//		root.left.left.left = new TreeNode(1);
+		//		root.left.left.right = new TreeNode(3);
+		//		root.left.right.left = new TreeNode(9);
+		//		root.left.right.right = new TreeNode(7);
+
+		//		TreeNode root = new TreeNode(2);
+
+		//		TreeNode root = new TreeNode(5);
+		//		root.left = new TreeNode(6);
+		//		root.right = new TreeNode(7);
+		//		root.left.left = new TreeNode(10);
+		//		root.left.right = new TreeNode(3);
+		//		root.right.left = new TreeNode(20);
+		//		root.right.right = new TreeNode(8);
+		//		root.left.left.left = new TreeNode(2);
+		//		root.left.left.right = new TreeNode(9);
+		//		root.left.right.left = new TreeNode(11);
+		//		root.left.right.right = new TreeNode(12);
+		//		root.right.right.left = new TreeNode(15);
+		//		root.right.right.right = new TreeNode(16);
+
+		//		TreeNode root = null;
+
 		List<List<Integer>> numLists = new BinaryTreeZigzagLevelOrderTraversal().zigzagLevelOrder(root);
+
 		for(List<Integer> numList : numLists) {
 			for(Integer num : numList) {
 				System.out.print(num + " ");
@@ -44,51 +58,52 @@ public class BinaryTreeZigzagLevelOrderTraversal {
 
 	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 
-		List<List<Integer>> nodeLists = new ArrayList<List<Integer>>();
+		List<List<Integer>> finalLists = new ArrayList<List<Integer>>();
 
 		if(root != null) {
-			Stack<TreeNode> currentList = null;
-			
-			Stack<TreeNode> nextList = new Stack<TreeNode>();
-			nextList.add(root);
-			
+
+			List<TreeNode> currentList = null;
+			List<TreeNode> nextLevelList = new ArrayList<TreeNode>();
+			nextLevelList.add(root);
+
 			int direction = 1;
-			
-			do {
-				currentList = nextList;
-				nextList = new Stack<TreeNode>();
-				
-				List<Integer> finalList = new ArrayList<Integer>();
-				
-				while(!currentList.isEmpty()) {
-					TreeNode node = currentList.pop();
-					
-					finalList.add(node.val);
-					if(direction == 1) {
-						if(node.left != null) {
-							nextList.add(node.left);
-						}
-						if(node.right != null) {
-							nextList.add(node.right);
-						}
+
+			while(!nextLevelList.isEmpty()) {
+
+				currentList = nextLevelList;
+				nextLevelList = new ArrayList<TreeNode>();
+
+				List<Integer> listToBeAdded = new ArrayList<Integer>();
+
+				Iterator<TreeNode> it = currentList.iterator();
+				while(it.hasNext()) {
+					TreeNode node = it.next();
+					listToBeAdded.add(node.val);
+					if(node.left != null) {
+						nextLevelList.add(node.left);
 					}
-					else if(direction == -1) {
-						if(node.right != null) {
-							nextList.add(node.right);
-						}
-						if(node.left != null) {
-							nextList.add(node.left);
-						}
+					if(node.right != null) {
+						nextLevelList.add(node.right);
 					}
 				}
-				
+
+				if(direction == 1) {
+					finalLists.add(listToBeAdded);
+				}
+				else {
+					List<Integer> listToBeAdded2 = new ArrayList<Integer>();
+
+					for(int i=listToBeAdded.size() - 1; i>=0; --i) {
+						listToBeAdded2.add(listToBeAdded.get(i));
+					}
+
+					finalLists.add(listToBeAdded2);
+				}
+
 				direction *= -1;
-				
-				nodeLists.add(finalList);
-				
-			} while(!nextList.isEmpty());
+			}
 		}
-		
-		return nodeLists;
+
+		return finalLists;
 	}
 }
