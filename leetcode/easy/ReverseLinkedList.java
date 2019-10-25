4c1e6e549bde15a7f7a41cbe1c0f5ffc88772c90
mohitkhosla01 <mohit.khosla.01@gmail.com>
2019-10-25 09:13:41 +0530

    Updated ReverseLinkedList

--- leetcode/easy/ReverseLinkedList.java
+++ leetcode/easy/ReverseLinkedList.java
@@ -6,20 +6,20 @@ public class ReverseLinkedList {
 
 	public static void main(String[] args) {
 
-			ListNode head = new ListNode(1);
-			head.next = new ListNode(2);
-			head.next.next = new ListNode(3);
-			head.next.next.next = new ListNode(4);
-			head.next.next.next.next = new ListNode(5);
+		//		ListNode head = new ListNode(1);
+		//		head.next = new ListNode(2);
+		//		head.next.next = new ListNode(3);
+		//		head.next.next.next = new ListNode(4);
+		//		head.next.next.next.next = new ListNode(5);
 
-		// ListNode head = null;
-		
-		// ListNode head = new ListNode(8);
-		// head.next = new ListNode(9);
-		
-		// ListNode head = new ListNode(20);
+		//		ListNode head = null;
 
-		ListNode node = new ReverseLinkedList().reverseList(head.next.next);
+		//		ListNode head = new ListNode(8);
+		//		head.next = new ListNode(9);
+
+		ListNode head = new ListNode(20);
+
+		ListNode node = new ReverseLinkedList().reverseList(head);
 
 		while(node != null) {
 			System.out.print(node.val + " ");
@@ -33,22 +33,18 @@ public class ReverseLinkedList {
 			return null;
 		}
 		else {
-			ListNode prevNode = null;
+			ListNode prev = null;
 			ListNode node = head;
+			ListNode next = null;
 
-			while(true) {
-				ListNode nextNode = node.next;
-				node.next = prevNode;
-				prevNode = node;
-				if(nextNode == null) {
-					break;
-				}
-				else {
-					node = nextNode;
-				}
+			while(node != null) {
+				next = node.next;
+				node.next = prev;
+				prev = node;
+				node = next;
 			}
-			
-			return node;
+
+			return prev;
 		}
 	}
 }
